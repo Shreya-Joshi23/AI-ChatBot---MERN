9fe812bd9971667112fd65b74e57cf8e2e56c080
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-08-29 11:19:08 +0530

    v1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import {config} from "dotenv"
-import morgan from 'morgan'
+// import morgan from 'morgan'
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import { limiter } from "./utils/rate-limiter.js";
@@ -14,7 +14,7 @@ app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 app.use("/api/v1",appRouter);
 
