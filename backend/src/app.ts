05cb29853c5d117be93ee70f577524a0815b5df0
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-09-02 10:26:18 +0530

    origin changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,26 @@
 import express from "express";
-import {config} from "dotenv"
+import { config } from "dotenv";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import { limiter } from "./utils/rate-limiter.js";
 import cors from "cors";
 
 config();
-const app=express();
+const app = express();
 
 //middlewares
-app.use(cors({origin:"https://ai-chat-bot-mern-hwlz.vercel.app/",credentials:true}))
+app.use(
+  cors({
+    origin: "https://ai-chatbot-mern-u8g3.onrender.com/",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use("/api/v1",appRouter);
+app.use("/api/v1", appRouter);
 
 export default app;
