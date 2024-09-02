3d011dfd51eb5b75512045246409040fe129f21d
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-09-02 10:27:49 +0530

    port changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
 
 //connections
-const port=process.env.PORT || 5174;
+const port=process.env.PORT || 3000;
 
 connectToDatabase().then(()=>{
   app.listen(port,()=>console.log(`Server Open on port ${port}`));
